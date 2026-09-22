4ee4517c257e75c0166af840b06f8bc8372678b4
Muhammad Novel <mnovel78@gmail.com>
2026-09-22 18:56:06 +0700

    tes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
             RolePermissionSeeder::class,
             ClassSeeder::class,
             AdminSeeder::class,
-            FullPilketosSeeder::class,
+            // FullPilketosSeeder::class,
         ]);
     }
 }
